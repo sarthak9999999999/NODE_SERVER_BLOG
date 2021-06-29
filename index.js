c80ac953ff3e4cfe8e1c263885f4ca11775cd2cc
miniproj-collab <sweetsarthak10@gmail.com>
2021-06-30 01:44:59 +0530

    ok

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const passport=require("passport");
-const { prependListener } = require("./models/users.models");
 const app=express();
 app.use(express.json());
 app.use(passport.initialize());
