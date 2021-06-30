ff3cbf75c1b2daffcd6519357460cf173f86b4c4
miniproj-collab <sweetsarthak10@gmail.com>
2021-06-30 19:27:26 +0530

    ok

--- routes/user.js
+++ routes/user.js
@@ -102,7 +102,7 @@ router.route("/login").post((req,res)=>{
             }
              if(result.password === req.body.password)
             {
-                const token =  jwt.sign({username : req.body.username}, configg.key,{
+                const token =  jwt.sign({username : req.body.username}, process.env.config_key,{
                     expiresIn : "24h"
                 });
                
